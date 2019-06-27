2b24fdeb9f74f0799bf7db2c6a1d332972d8b7d1
Dunteng <lindunteng@gmail.com>
2019-06-27 17:14:49 +0800

    实现验证码登录功能

--- ele-app/src/router.js
+++ ele-app/src/router.js
@@ -1,18 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from './views/index.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'index',
-      component: Index
+      component: ()=>import('./views/index.vue')
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: ()=>import('./views/Login.vue')
+    }
 
   ]
 })
+//路由守卫
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.ele_login ? true : false;
+  if (to.path == '/login') {
+    next();
+  } else {
+    // 是否在登录状态下
+    isLogin ? next() : next('/login');
+  }
+});
+
+export default router;
